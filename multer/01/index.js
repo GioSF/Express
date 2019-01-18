88656ac01172855c32ed57f733851eb7af8c795f
GioSF <giosfdev@gmail.com>
2019-01-18 19:12:27 -0200

    Express-Multer: fileValidation, magicNumbers, size

--- multer/01/index.js
+++ multer/01/index.js
@@ -2,25 +2,12 @@ const express = require('express');
 const app = express();
 const multer = require('multer');
 var storage = multer.memoryStorage();
+const fs = require('fs');
 
 //É possível passar um diretório para o multer
 //const upload = multer({ dest: 'uploads/' });
-var upload = multer({ storage : storage });
+var upload = multer({ storage: storage });
 const buffer = require('buffer');
-
-//O storage permite definir o nome do arquivo.
-
-/* var storage = multer.diskStorage({
-    //onde salvar. Pode fazer tratamento, criar pasta, chamar funções assíncronas, tudo antes do callback
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-    },
-    //Qual o nome do arquivo, por exemplo, puxar no bd
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname);
-    }
-})
- */
 
 app.set('view engine', 'ejs')
 
@@ -29,14 +16,26 @@ app.get('/', (req, res) => res.render('home'))
 app.post('/', upload.single('img'), (req, res) => {
     let fileBuf = new Buffer.alloc(4, req.file.buffer, 'utf8');
     console.log(req.file.mimetype);
-    if(fileBuf.toString() == 'OggS'){
-        res.send('Ogg');
+    if (fileBuf.toString() == 'OggS') {
+        if (req.file.size > 4000000) {
+            res.send('Arquivo muito grande');
+        } else {
+            console.log(req.file.size);
+            res.send('Ogg');
+            fs.writeFile(`uploads/${req.file.originalname}`, req.file.buffer, function (err) {
+                if (err) throw err;
+                console.log(err);
+            })
+        }
     } else {
         res.send('Não Ogg');
     }
     console.log(fileBuf.toString());
-//    res.send('Arquivo Salvo');
+    //    res.send('Arquivo Salvo');
+
+
 })
+
 
 app.listen(3000, () => console.log('running...'))
 
