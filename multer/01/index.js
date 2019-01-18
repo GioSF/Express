2d103d4b8c6ec899904f6bd641bd0c7da6ad1cfa
GioSF <giosfdev@gmail.com>
2019-01-18 19:15:43 -0200

    Express-Multer: fileValidation, magicNumbers, size

--- multer/01/index.js
+++ multer/01/index.js
@@ -4,8 +4,6 @@ const multer = require('multer');
 var storage = multer.memoryStorage();
 const fs = require('fs');
 
-//É possível passar um diretório para o multer
-//const upload = multer({ dest: 'uploads/' });
 var upload = multer({ storage: storage });
 const buffer = require('buffer');
 
@@ -18,25 +16,21 @@ app.post('/', upload.single('img'), (req, res) => {
     console.log(req.file.mimetype);
     if (fileBuf.toString() == 'OggS') {
         if (req.file.size > 4000000) {
-            res.send('Arquivo muito grande');
+            res.send('File bigger than 4Mb');
         } else {
             console.log(req.file.size);
-            res.send('Ogg');
+            res.send('Ogg file.');
             fs.writeFile(`uploads/${req.file.originalname}`, req.file.buffer, function (err) {
                 if (err) throw err;
                 console.log(err);
             })
+            res.send('File saved.');
         }
     } else {
-        res.send('Não Ogg');
+        res.send('Not a Ogg file.');
     }
     console.log(fileBuf.toString());
-    //    res.send('Arquivo Salvo');
-
 
 })
 
-
 app.listen(3000, () => console.log('running...'))
-
-//fonte base: https://www.devpleno.com/multer-upload-de-imagens-com-nodejs-e-express/
